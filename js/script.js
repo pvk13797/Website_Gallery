aa5d15befaf938ce5548ce498874883eb678e8ab
Prasanna Venkatesh Kumar RTV <pvk13797@gmail.com>
2021-08-20 14:29:30 +0530

    Responsive Updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const toggleMenu = document.getElementsByClassName('menu_bar')[0],
-    headerMenu = document.getElementsByClassName('top_nav')[0];
+    headerMenu = document.getElementsByClassName('top_nav')[0],
+    closeMenu = document.getElementsByClassName('content_area')[0];
 
     toggleMenu.addEventListener("click", () => {
         toggleMenu.classList.toggle("active");
         headerMenu.classList.toggle("active");
-    })
+    });
 
-
+    closeMenu.addEventListener("click", () => {
+        if(toggleMenu.classList.toggle("active") !== "active" && headerMenu.classList.toggle("active") !== "active") {
+            toggleMenu.classList.remove("active");
+            headerMenu.classList.remove("active");
+        }
+    });
