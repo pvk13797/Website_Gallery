877a8b3ab18ae8c7b1b90f16a16b44b02d2e3d78
Prasanna Venkatesh Kumar RTV <pvk13797@gmail.com>
2021-08-25 17:35:22 +0530

    Responsive Updated

--- js/script.js
+++ js/script.js
@@ -17,3 +17,59 @@ const toggleMenu = document.getElementsByClassName('menu_bar')[0],
             console.log("No Menus");
         }
     });
+
+function openModel() {
+    document.getElementsByClassName("box")[0].style.display = "block";
+}
+
+function clickClose() {
+    document.getElementsByClassName("box")[0].style.display = "none";
+}
+
+const slides = document.getElementsByClassName("mySlides"),
+    nextBtn = document.getElementById("next"),
+    prevBtn = document.getElementById("prev");
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    var newIndex = (slideIndex += n);
+    showDisabled(newIndex);
+    showSlides(newIndex);
+}
+
+function currentSlide(n) {
+    var newIndex = (slideIndex = n);
+    showDisabled(newIndex);
+    showSlides(newIndex);
+}
+
+function showSlides(n) {
+    var i;
+
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
+
+function showDisabled(newIndex) {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+
+    if(newIndex === slides.length) {
+        nextBtn.disabled = true;
+    } else if(newIndex === 1) {
+        prevBtn.disabled = true;
+    }
+}
